d458b06146f6269dff22ed60ff75383d36c9f476
Gustavo Henrique <gusgegomes@hotmail.com>
2023-07-06 18:10:34 -0300

    ajuste de validação

--- src/main/java/InnoRocket/Controller/EspecializacaoController.java
+++ src/main/java/InnoRocket/Controller/EspecializacaoController.java
@@ -4,11 +4,12 @@ import InnoRocket.DAO.EspecioalizacaoDAO;
 import InnoRocket.Model.Especializacao;
 import javax.swing.*;
 import java.util.List;
+import java.util.regex.Pattern;
 
 
 public class EspecializacaoController {
     public static void cadastrar() throws ClassNotFoundException {
-        String nome = JOptionPane.showInputDialog("Digite a nome da Especializacao: ");
+        String nome = getValidNome(null);
         Especializacao especializacao = new Especializacao(null, nome);
         EspecioalizacaoDAO.salvar(especializacao);
     }
@@ -20,7 +21,7 @@ public class EspecializacaoController {
                 "Processo", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
         List<Especializacao> especializacao = EspecioalizacaoDAO.buscaPorNome((String) selection);
 
-        String nome = JOptionPane.showInputDialog("Digite a nome da Especializacao: ", especializacao.get(0).getNome());
+        String nome = getValidNome(null);
 
         Especializacao especializacaoAlterar = new Especializacao(especializacao.get(0).getEspecializacaoId(), nome);
         EspecioalizacaoDAO.alterar(especializacaoAlterar);
@@ -33,5 +34,25 @@ public class EspecializacaoController {
                 "Processo", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
         List<Especializacao> especializacao = EspecioalizacaoDAO.buscaPorNome((String) selection);
         EspecioalizacaoDAO.excluir(especializacao.get(0));
+    }
+
+    private static String getValidNome(String nome) {
+        do {
+            nome = JOptionPane.showInputDialog("Digite a nome da Especializacao ");
+            if (nome == null) {
+                return null;
+            }
+            if (nome.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "O nome não pode ser vazio. Por favor, digite um nome válido.");
+            } else if (containsSpecialCharacters(nome)) {
+                JOptionPane.showMessageDialog(null, "O nome não pode conter caracteres especiais. Por favor, digite um nome válido.");
+            }
+        } while (nome.isEmpty() || containsSpecialCharacters(nome));
+        return nome;
+    }
+
+    private static boolean containsSpecialCharacters(String text) {
+        Pattern specialChars = Pattern.compile("[^a-zA-Z0-9]");
+        return specialChars.matcher(text).find();
     }
 }
