3fe21366a3985cf25fc863717d0c8cafec2afdee
Marcos Vinicius Cardoso <mvinicius@narwalsistemas.com.br>
2023-07-06 23:12:27 -0300

    Alterado index relatório Centro

--- src/main/java/InnoRocket/Form/RelatorioCentroForm.java
+++ src/main/java/InnoRocket/Form/RelatorioCentroForm.java
@@ -20,7 +20,7 @@ public class RelatorioCentroForm extends JPanel {
     private static final long serialVersionUID = 1L;
 
     public static final String[] nomeColunas =
-            {"Nome", "Rua", "CEP", "Número", "Bairro", "Complemento", "Cidade", "Status", "Redes Sociais", "Foto", "Data de Cadastro", "Data de Criação", ""};
+            {"Id", "Nome", "Rua", "CEP", "Número", "Bairro", "Complemento", "Cidade", "Status", "Redes Sociais", "Foto", "Data de Cadastro", "Data de Criação", ""};
 
     protected JTable table;
     protected JScrollPane scroller;
